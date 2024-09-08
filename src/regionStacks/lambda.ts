7e0a5a42c7132891447c39f29f83c8ad4bfb0c93
Court Schuett <schuettc@amazon.com>
2024-09-08 15:09:06 -0500

    fix: remove sns

--- src/regionStacks/lambda.ts
+++ src/regionStacks/lambda.ts
@@ -2,7 +2,6 @@ import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { ITopic } from 'aws-cdk-lib/aws-sns';
-import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 import { Construct } from 'constructs';
 
 export class LambdaResources extends Construct {
